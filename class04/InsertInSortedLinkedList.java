e7a2334c1da7c5b17172fc1a19b3776e07f38de5
Shiyang Li <sli99@hawk.iit.edu>
2018-07-09 23:11:00 -0500

    insertSortedLinkedList

---
+++ class04/InsertInSortedLinkedList.java
@@ -0,0 +1,23 @@
+public class Solution {
+  public ListNode insert(ListNode head, int value) {
+      // Write your solution here
+      //    x -> y -> z -> w -> null
+      //   1    2            3
+      //   c   c.next
+      // case 1: head == null || target <= head.value  insert before the head
+      // case 2: cur.next != null || cur.next.value >= value  insert between cur and cur.next
+      ListNode newNode = new ListNode(value);
+      if (head == null || head.value >= value) {
+         newNode.next = head;
+         return newNode;
+      }
+      ListNode cur = head;
+      while (cur.next != null && cur.next.value < value) {
+        cur = cur.next;
+      }
+      ListNode next = cur.next;
+      cur.next = newNode;
+      newNode.next = next;
+      return head;
+  }
+}
