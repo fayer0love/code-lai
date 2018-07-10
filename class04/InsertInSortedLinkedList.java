024f0f96569cca883757c4402248bfea227a1db1
Shiyang Li <sli99@hawk.iit.edu>
2018-07-09 23:17:37 -0500

    add problem desciption

--- class04/InsertInSortedLinkedList.java
+++ class04/InsertInSortedLinkedList.java
@@ -1,3 +1,25 @@
+/**
+Insert In Sorted Linked List
+Description
+Insert a value in a sorted linked list.
+
+Examples
+
+L = null, insert 1, return 1 -> null
+L = 1 -> 3 -> 5 -> null, insert 2, return 1 -> 2 -> 3 -> 5 -> null
+L = 1 -> 3 -> 5 -> null, insert 3, return 1 -> 3 -> 3 -> 5 -> null
+L = 2 -> 3 -> null, insert 1, return 1 -> 2 -> 3 -> null
+
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+ 
 public class Solution {
   public ListNode insert(ListNode head, int value) {
       // Write your solution here
